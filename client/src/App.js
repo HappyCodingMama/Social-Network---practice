09491a447b602be3eac8e40deedb4e1ddc924731
HappyCodingMama <67956944+HappyCodingMama@users.noreply.github.com>
2020-12-09 22:53:25 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React, { Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
 const App = () => (
   <Fragment>
     <Navbar />
-    <Landing />
+    <Route exact path='/' component={Landing} />
+    <section className="container">
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </section>
   </Fragment>
 );
 
