ba20c0d01d949c6334828e66d1e38dae1ecb4a36
HappyCodingMama <happycodingmama@gmail.com>
2020-12-10 23:39:19 +0900

    add tags

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
@@ -6,16 +7,18 @@ import Login from './components/auth/Login';
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Route exact path='/' component={Landing} />
-    <section className="container">
-      <Switch>
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-      </Switch>
-    </section>
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
